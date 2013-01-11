009fb760282476d2080bace4e956cb3b59fa8311
Julio Pavon <julio@jpavon.com>
2013-01-11 17:22:19 +0000

    structure change, v1

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,7 @@ module.exports = function(grunt) {
  grunt.loadNpmTasks('grunt-contrib-uglify');
  grunt.loadNpmTasks('grunt-contrib-concat');
  grunt.loadNpmTasks('grunt-contrib-copy');
+ grunt.loadNpmTasks('grunt-usemin');
  grunt.loadNpmTasks('grunt-compass');
 
  // registerTasks
